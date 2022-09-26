3c41e954d24d8b774124f8ca4899fd6f9c242725
Timothy Holmes <tim.a.holmes@gmail.com>
2022-09-26 10:29:35 +1000

    fixed typo in script

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@
 curl -m 10 http://health.hh.home/ping/9ca9609e-474c-4b02-a24c-e3e1ba88ee35/start
 
 # Payload here:
-python /bom-bot/bom-bot.py
+python /bom-bot/bom_bot.py
 
 # Finished ping
 curl -m 10 --retry 5 http://health.hh.home/ping/9ca9609e-474c-4b02-a24c-e3e1ba88ee35/$?
